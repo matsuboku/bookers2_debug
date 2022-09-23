aedd49142cd668ef272d7abcd48cb0d7ee7107f4
EC2 Default User <ec2-user@ip-172-31-25-252.ap-northeast-1.compute.internal>
2022-09-23 07:30:57 +0000

    [Finish]コメント機能

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,12 +5,14 @@ class BooksController < ApplicationController
     @book = Book.find(params[:id])
     @user = @book.user
     @book_new = Book.new
+    @book_comment = BookComment.new
   end
 
   def index
     @books = Book.all
     @book_new = Book.new
-    
+
+
   end
 
   def create
--- app/models/book.rb
+++ app/models/book.rb
@@ -4,6 +4,7 @@ class Book < ApplicationRecord
   validates :body,presence:true,length:{maximum:200}
 
   has_many :favorites, dependent: :destroy
+  has_many :book_comments, dependent: :destroy
   # has_many :favorite_users, through: :favorites, source: :user
   def favorited_by?(user)
     favorites.where(user_id: user.id).exists?
